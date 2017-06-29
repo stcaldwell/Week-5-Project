dfcb50e0fe4529538321811a957acbe63eb85c8d
Sam Caldwell <caldwell.s.thorne@gmail.com>
2017-06-29 15:02:07 -0400

    add functioning hangman game.

--- app.js
+++ app.js
@@ -28,10 +28,11 @@ app.use(session({
   saveUninitialized: true
 }));
 
+let guessesLeft = 8;
 const selections = [];
 let guess;
 let letterGuess = [];
-var wordChoices = words.filter(function(word) {return word.length <= 5; });
+var wordChoices = words.filter(function(word) {return word.length <= 8; });
 var randomWord = wordChoices[Math.floor(Math.random() * wordChoices.length)];
 var randomWordList = randomWord.split('');
 var arrLength = randomWordList.length;
@@ -48,41 +49,77 @@ for (var i = 0; i < randomWord.length; i++) {
 }
 
 // Checks the guess of the user to each letter in the random word. If guess by the user is correct it will then replace the underscore with the letter.
+
+let alreadyGuessed = false;
 function checkGuess (req, res) {
   for(i = 0; i < randomWord.length; i++){
     if (guess === randomWord[i]) {
-      letterGuess[i] = guess;
+      letterGuess[i] = guess
       console.log(letterGuess);
     }
   }
+
+  // let alreadyGuessed = false;
+  for (var i=0; i < selections.length; i++) {
+    if(guess === selections[i]) {
+      alreadyGuessed = true;
+    }
+  }
+  if (alreadyGuessed === false && randomWord.indexOf(guess) === -1) {
+    selections.push(guess);
+    guessesLeft--;
+  }
+
   return letterGuess;
-  selections.push(guess)
+
 }
-console.log(letterGuess);
-console.log(selections);
+
+
+// console.log(letterGuess);
+// console.log(selections);
 
 
 // var randomWord = function(min, max) {
 //   return Math.floor(Math.random() * (max - min + 1) + min);
 // };
 
-
 // use router.get instead of app.get to make it link together.
 
 
 app.get('/', function (req, res){
-  res.render('index', {letterGuess: letterGuess, selections: selections})
+  res.render('index', {letterGuess: letterGuess, selections: selections, guessesLeft: guessesLeft})
 });
 
 app.post('/', function(req, res){
   var guesses = req.body.letters.toLowerCase();
   guess = guesses;
   checkGuess(guess);
-  selections.push(guess);
-  res.render('index', {letterGuess: letterGuess, selections: selections})
+  if(guessesLeft === 0) {
+    res.redirect('/lose');
+  }
+  let pickedWord = randomWordList.join(',');
+  let finalGuessedWord = letterGuess.join(',');
+  if(pickedWord === finalGuessedWord) {
+    res.redirect('/win');
+  }
+  res.render('index', {letterGuess: letterGuess, selections: selections, guessesLeft: guessesLeft})
   console.log(selections)
 })
 
+app.get('/win', function(req, res){
+  res.render('win');
+})
+
+// app.post('/win', function(req, res){
+//
+// })
+app.get('/lose', function(req, res){
+  res.render('lose');
+})
+
+// app.post('/lose', function(req,res){
+//
+// })
 
 app.listen(3000, function(){
   console.log('It has begun!')
